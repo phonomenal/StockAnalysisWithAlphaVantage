f792c00636b7e7e193a65ccc8f2edbad74a7571d
james-leha <james-leha@outlook.com>
2020-03-26 12:53:26 -0500

    updated settings api key reference

--- TimeSeriesPandaOutput.py
+++ TimeSeriesPandaOutput.py
@@ -1,5 +1,6 @@
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
-ts = TimeSeries(key='4PN8G24M5N3NI7GQ', output_format='pandas')
+from Environment_Settings import settings
+ts = TimeSeries(key=settings.API_KEY, output_format='pandas')
 data, meta_data = ts.get_intraday(symbol='GOOG',interval='1min', outputsize='full')
 pprint(data.head(2))
