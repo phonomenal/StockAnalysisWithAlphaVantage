42ee110b8c97cd275d4468ac238eb8b9a390411a
james-leha <james-leha@outlook.com>
2020-03-27 16:44:02 -0500

    format stock result for mongo db, create function to insert

--- AzureCosmoDB_Insert.py
+++ AzureCosmoDB_Insert.py
@@ -14,9 +14,17 @@ mycol = mydb["AlphaVantageLists"]
 
 mydict = {"symbol": "Ivan", "name": "Le", "amount": 37.60 }
 
-myStockDict = {'1. open': 33.73, '2. high': 33.9, '3. low': 32.7, '4. close': 33.03, '5. volume': 78700076.0, 'symbol': 'AMD', 'username': 'jameha', 'date': new
-Date(2019, 10, 29)}
+myStockDict = {'1. open': 33.73, '2. high': 33.9, '3. low': 32.7, '4. close': 33.03, '5. volume': 78700076.0, 'symbol': 'AMD', 'username': 'jameha', 'date': '2019-10-29'}
 
-x = mycol.insert_one(myStockDict)
+myStockDictMultiple = [{'1_open': 46.32, '2_high': 47.975, '3_low': 45.9, '4_close': 46.58, '5_volume': 74273700.0, 'symbol': 'AMD', 'username': 'jameha', 'date': '2020-03-27'}, {'1_open': 45.78, '2_high': 47.5, '3_low': 45.4, '4_close': 47.5, '5_volume': 73276429.0, 'symbol': 'AMD', 'username': 'jameha', 'date': '2020-03-26'}, {'1_open': 46.79, '2_high': 47.875, '3_low': 44.425, '4_close': 44.63, '5_volume': 93760389.0, 'symbol': 'AMD', 'username': 'jameha', 'date': '2020-03-25'}]
 
-print("Item inserted to collection with id: " + x.inserted_id)
+# x = mycol.insert_many(myStockDictMultiple)
+
+class AzureCosmosCRUD() :
+    
+    def __init__(self, dataObjectReturned):
+        self.dataObjectReturned = dataObjectReturned
+
+    def insertDataToMongo(self):
+        
+        x = mycol.insert_many(self.dataObjectReturned)
--- TimeSeries_GetDaily_POC.py
+++ TimeSeries_GetDaily_POC.py
@@ -2,6 +2,7 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 from Environment_Settings import settings
 import json
+import AzureCosmoDB_Insert
 
 #Name of User, for tracking purposes
 userName = 'jameha'
@@ -11,7 +12,7 @@ userName = 'jameha'
 stockSymbol = 'AMD'
 
 #Number of rows you want returned from the query
-rowsOfData = 104
+rowsOfData = 25
 
 ts = TimeSeries(key=settings.API_KEY, output_format='pandas')
 if rowsOfData > 100:
@@ -27,18 +28,29 @@ if dataWeeksReturned.shape[0] == rowsOfData:
     dataDatesReturned = dataWeeksReturned._stat_axis.date
 
     #Convets Panda Data Frame to dictionary object
-    dObject = dataWeeksReturned.to_dict(orient='records')
+    dataObjectReturned = dataWeeksReturned.to_dict(orient='records')
 
     i = 0
-    for entry in dObject:
+    for entry in dataObjectReturned:
+        entry['1_open'] = entry.pop('1. open')
+        entry['2_high'] = entry.pop('2. high')
+        entry['3_low'] = entry.pop('3. low')
+        entry['4_close'] = entry.pop('4. close')
+        entry['5_volume'] = entry.pop('5. volume')
         entry['symbol'] = stockSymbol
         entry['username'] = userName
         entry['date'] = str(dataDatesReturned[i])
         i += 1
 
     pprint(dataWeeksReturned)
+    print(dataObjectReturned)
 
-    print(dObject)
+    #Insert results to Cosmos DB
+
+    AzCosmos = AzureCosmoDB_Insert.AzureCosmosCRUD(dataObjectReturned)
+
+    AzCosmos.insertDataToMongo()
+
 else:
     print("Rows of data requested did not match what was returned, /n Ensure you are using either compact or full outputsizes /n Exiting")
     exit()
