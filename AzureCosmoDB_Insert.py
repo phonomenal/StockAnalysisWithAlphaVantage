af11c82b776b31a49754f400fc6b8fddb0306bee
james-leha <james-leha@outlook.com>
2020-03-27 20:58:12 -0500

    mongoDB process query, list and find max/min

--- AzureCosmoDB_Insert.py
+++ AzureCosmoDB_Insert.py
@@ -20,6 +20,33 @@ myStockDictMultiple = [{'1_open': 46.32, '2_high': 47.975, '3_low': 45.9, '4_clo
 
 # x = mycol.insert_many(myStockDictMultiple)
 
+query_GetUserStockGtDate = { "username": "jameha", "symbol":"AMD", "date": { "$gt" : "2020-03-03"} }
+
+#use aggreate method to sort queries
+results_SortedByAsc = mycol.aggregate([{
+    "$match": query_GetUserStockGtDate
+},
+{
+    "$sort": {
+        "2_high": pymongo.ASCENDING
+    }
+}])
+
+#Convert Cursor Deque to List
+results_List = list(results_SortedByAsc)
+
+result_MaxHigh = results_List[-1]
+
+results_MinHigh = results_List[0]
+
+print("The max high price during this time frame was on: " + result_MaxHigh['date'])
+print("At the price of: " + str(result_MaxHigh['2_high']))
+
+print("The min high price during this time frame was on: " + results_MinHigh['date'])
+print("At the price of: " + str(results_MinHigh['2_high']))
+
+
+
 class AzureCosmosCRUD() :
     
     def __init__(self, dataObjectReturned):
---
+++ AzureCosmosDB_Process.py
@@ -0,0 +1,39 @@
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.errors as errors
+import azure.cosmos.http_constants as http_constants
+import pymongo
+from Environment_Settings import settings
+import os
+import datetime
+
+uri = settings.DB_CONNECTION_STRING
+client = pymongo.MongoClient(uri)
+
+mydb = client["AlphaVantageDataBase"]
+mycol = mydb["AlphaVantageLists"]
+
+#query we pass to mongodb
+query_GetUserStockGtDate = { "username": "jameha", "symbol":"AMD", "date": { "$gt" : "2020-03-03"} }
+
+#use aggreate method to sort queries
+results_SortedByAsc = mycol.aggregate([{
+    "$match": query_GetUserStockGtDate
+},
+{
+    "$sort": {
+        "2_high": pymongo.ASCENDING
+    }
+}])
+
+#Convert Cursor Deque to List
+results_List = list(results_SortedByAsc)
+
+result_MaxHigh = results_List[-1]
+
+results_MinHigh = results_List[0]
+
+print("The max high price during this time frame was on: " + result_MaxHigh['date'])
+print("At the price of: " + str(result_MaxHigh['2_high']))
+
+print("The min high price during this time frame was on: " + results_MinHigh['date'])
+print("At the price of: " + str(results_MinHigh['2_high']))
